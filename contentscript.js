d468c9c57814f91df71fb830186fe03bbd6714e2
Calvin Cheung <calvin@topprospect.com>
2012-02-10 00:11:41 -0800

    added some comments

--- contentscript.js
+++ contentscript.js
@@ -9,14 +9,18 @@
   
   var injectSearch = function() {
     var count = $("div.pin").length;
+    // Do only if there are new pins on the page.
     if (count > numPins) {
       numPins = count;
       
+      // Find all pins without the search button.
       var newPins = $("div.pin").filter(function() {
         var $el = $(this);
         return $el.find("> .actions").length && $el.find("> a.ImgLink > img").length &&
-          $el.find("> .actions > .ext-search-container").length == 0;
+          $el.find("> .actions > .ext-search-container").length === 0;
       });
+      
+      // Append search button with image url.
       newPins.each(function() {
         var $el = $(this);
         $el.find("> .actions").append(new String(searchButton).replace("#IMAGE_URL#",
